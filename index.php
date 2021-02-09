f6f64eee2126b8aefe887c216ae50aac2d2dc12a
KilmarG <kilmar.999@gmail.com>
2021-02-08 20:19:44 -0600

    Commit All

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+//se incluye codigo de archivos de otras carpetas 
+//esto permite reciclar codigo
+include_once "controllers/template.controller.php";
+
+$template = new ControllerTemplate();
+$template -> ctrTemplate();
