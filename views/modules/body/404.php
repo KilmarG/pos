6f6ff7e720e349174cae4c6e126a2970c84f5829
KilmarG <kilmar.999@gmail.com>
2021-02-11 16:08:33 -0600

    Pagina de error 404

---
+++ views/modules/body/404.php
@@ -0,0 +1,6 @@
+<!-- Site wrapper - Contenedor del Sitio -->
+<div class="wrapper">
+    <?php 
+
+    ?>
+</div>
--- views/modules/body/sitewrapper.php
+++ views/modules/body/sitewrapper.php
@@ -1,9 +1,11 @@
 <!-- Site wrapper - Contenedor del Sitio -->
 <div class="wrapper">
     <?php 
+        //---------------------------------------//
         include "navbar.php"; //barra de navegación.
+        //---------------------------------------//
         include "sidebar.php"; //barra lateral.
-
+        //---------------------------------------//
         if(isset($_GET["ruta"])){
             if( $_GET["ruta"] == "home"         ||
                 $_GET["ruta"] == "users"        ||
@@ -13,10 +15,20 @@
                 $_GET["ruta"] == "create-sales" ||
                 $_GET["ruta"] == "sales-report")
                 {
-                    include "crud/".$_GET["ruta"].".php";   //contenido de la pagina.
+                    include "crud/".$_GET["ruta"].".php";   
                 }
-        }
+                else {
+                    //se incluye codigo de archivos de otras carpetas 
+                    //esto permite reciclar codigo
+                    include_once "controllers/404.controller.php";
+
+                    $template = new ControllerTemplate();
+                    $template -> ctrTemplate();
+                }
+        } //contenido de la pagina.
+        //---------------------------------------//
         include "footer.php"; //pie de pagina.
+        //---------------------------------------//
     ?>
 </div>
 
