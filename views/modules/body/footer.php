be022db1b62d2829c7146a46dc6ce332370d0e0f
Kilmar Eliezer García Pacheco <68747653+KilmarG@users.noreply.github.com>
2021-02-11 16:22:56 -0600

    Delete views directory

--- views/modules/body/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 0.0.1
-    </div>
-    <strong>Copyright &copy; 2021 <a href="https://adminlte.io">AdminLibreria</a>.</strong> Todos los derechos reservador.
-</footer>
